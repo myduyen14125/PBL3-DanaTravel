7dc6928c2003d7da4caa85db83feb7ae85a38098
daitoan2000@gmail.com <daitoan2000@gmail.com>
2022-04-11 23:44:40 +0700

    Update Change-password

--- PBL3/View/login/ChangePassword.cs
+++ PBL3/View/login/ChangePassword.cs
@@ -26,56 +26,111 @@ namespace PBL3
 
         private void newPassword_Click(object sender, EventArgs e)
         {
-            txtPass.Text = "";
-            txtPass.PasswordChar = '*';
+            if (txtPass.Text == "New password")
+            {
+                txtPass.Text = "";
+                txtPass.PasswordChar = '*';
+            }
+            if (txtConfirm.Text == "")
+            {
+                txtConfirm.Text = "Confirm password";
+                txtConfirm.PasswordChar = '\0';
+            }
+            if (txtCode.Text == "")
+            {
+                txtCode.Text = "Code";
+            }
         }
 
         private void txtConfirm_Click(object sender, EventArgs e)
         {
-            txtConfirm.Text = "";
-            txtConfirm.PasswordChar = '*';
+            if (txtConfirm.Text == "Confirm password")
+            {
+                txtConfirm.Text = "";
+                txtConfirm.PasswordChar = '*';
+            }
+
+            if (txtPass.Text == "")
+            {
+                txtPass.Text = "New password";
+                txtPass.PasswordChar = '\0';
+            }
+            if (txtCode.Text == "")
+            {
+                txtCode.Text = "Code";
+            }
         }
 
         private void txtCode_Click(object sender, EventArgs e)
         {
-            txtCode.Text = "";
-        
+            if (txtCode.Text == "Code")
+            {
+                txtCode.Text = "";
+            }
+            if (txtPass.Text == "")
+            {
+                txtPass.Text = "New password";
+                txtPass.PasswordChar = '\0';
+            }
+            if (txtConfirm.Text == "")
+            {
+                txtConfirm.Text = "Confirm password";
+                txtConfirm.PasswordChar = '\0';
+            }
         }
 
         private void btnChange_Click(object sender, EventArgs e)
         {
             string password = txtPass.Text.Trim();
             string confirmPass = txtConfirm.Text.Trim();
-            int code = Convert.ToInt32(txtCode.Text.Trim());
-
-            if (string.IsNullOrEmpty(password))
+            
+            if(!ValidateFormChange())
             {
-                MessageBox.Show("Password is empty. Please re-enter password");
-                txtPass.Focus();
-            }
-            if (string.IsNullOrEmpty(confirmPass))
-            {
-                MessageBox.Show("Confirm Password is empty. Please re-enter confirm password");
-                txtConfirm.Focus();
-            }
-            if (!confirmPass.Equals(password))
-            {
-                MessageBox.Show("Confirm Password didn't match. Try again");
-                txtConfirm.Focus();
-            }
-            if (code != this.code)
-            {
-                MessageBox.Show("Verified code didn't match. Please check your email to get the verified code");
+                return;
             }
 
-
-            if (AccountBUS.Instance.ChangePassword(new Account(email, HashPassword.GetHash(password))))
+            if (AccountBUS.Instance.ChangePassword(new Account(email, HashPassword.GetHash(txtPass.Text))))
             {
                 this.Hide();
                 LoginForm f = new LoginForm();
                 f.Closed += (s, args) => this.Close();
                 f.Show();
             }
+        }
+        private bool ValidateFormChange()
+        {
+            if (string.IsNullOrEmpty(txtPass.Text) || txtPass.Text == "New password")
+            {
+                MessageBox.Show("Password is empty. Please re-enter password");
+                txtPass.Focus();
+                return false;
+            }
+            if (string.IsNullOrEmpty(txtConfirm.Text) || txtConfirm.Text == "Confirm password")
+            {
+                MessageBox.Show("Confirm Password is empty. Please re-enter confirm password");
+                txtConfirm.Focus();
+                return false;
+            }
+            if (!txtConfirm.Text.Equals(txtPass.Text))
+            {
+                MessageBox.Show("Confirm Password didn't match. Try again");
+                txtConfirm.Focus();
+                return false;
+            }
+
+            if(txtCode.Text == "")
+            {
+                MessageBox.Show("Code is empty. Please re-enter code");
+                txtCode.Focus();
+                return false;
+            }
+            int code = Convert.ToInt32(txtCode.Text.Trim());
+            if (code != this.code)
+            {
+                MessageBox.Show("Verified code didn't match. Please check your email to get the verified code");
+                return false;
+            }
+            return true;
         }
     }
 }
